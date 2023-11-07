35df2553714be55f6b20f6244e9335fe874d96cb
AliyaDevNova <124171140+aliyabatoolalvi@users.noreply.github.com>
2023-11-07 22:57:12 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("calculateAge").addEventListener("click", function () {
+    const birthDay = parseInt(document.getElementById("birthDay").value);
+    const birthMonth = parseInt(document.getElementById("birthMonth").value);
+    const birthYear = parseInt(document.getElementById("birthYear").value);
+    if (isNaN(birthDay) || isNaN(birthMonth) || isNaN(birthYear)) {
+        document.getElementById("result").textContent = "Please enter valid date values.";
+        return;
+    }
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1; // Months are 0-indexed
+    const currentDay = currentDate.getDate();
+
+    let years = currentYear - birthYear;
+    let months = currentMonth - birthMonth;
+    let days = currentDay - birthDay;
+
+    if (days < 0) {
+        months--;
+        days += new Date(currentYear, currentMonth - 1, 0).getDate();
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    document.getElementById("result").textContent = `Your age is ${years} years, ${months} months, and ${days} days.`;
+});
